2bbb06b651ed44ff1aa7e811c67f3fc9f9c484ae
mferris <mferris>
2003-06-02 22:22:49 +0000

    Standardized file includes.

---
+++ PBTemplates/MOKit Class.pbfiletemplate/class.h
@@ -0,0 +1,45 @@
+// «FILENAME»
+// MOKit
+//
+// Copyright © «YEAR», «FULLUSERNAME».  All rights reserved.
+// See bottom of file for license and disclaimer.
+
+/*!
+ @header «FILEBASENAME»
+ @discussion Defines the «FILEBASENAMEASIDENTIFIER» class.
+ */
+
+
+#if !defined(__MOKIT_«FILEBASENAMEASIDENTIFIER»__)
+#define __MOKIT_«FILEBASENAMEASIDENTIFIER»__ 1
+
+#import <Cocoa/Cocoa.h>
+#import <MOKit/MOKitDefines.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+    
+/*!
+ @class «FILEBASENAMEASIDENTIFIER»
+ @abstract ABSTRACT GOES HERE
+ @discussion «FILEBASENAMEASIDENTIFIER» is, like, um, really cool, ya know.
+ */
+@interface «FILEBASENAMEASIDENTIFIER» : NSObject {
+
+}
+
+@end
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // __MOKIT_«FILEBASENAMEASIDENTIFIER»__
+
+
+/*
+ This file contains Original Code and/or Modifications of Original Code as defined in and that are subject to the Ferris Public Source License Version 1.2 (the 'License'). You may not use this file except in compliance with the License. Please obtain a copy of the License at http://mokit.sourceforge.net/License.html and read it before using this file.
+
+ The Original Code and all software distributed under the License are distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND MIKE FERRIS HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT. Please see the License for the specific language governing rights and limitations under the License.
+ */
